edb34a2bf4226124b9dfca99e7e6d341f3ead70f
Venuspatel711 <160982867+Venuspatel711@users.noreply.github.com>
2024-03-28 19:20:07 +0000

    i have added some final changes

--- MyDecorator/Plain.cs
+++ MyDecorator/Plain.cs
@@ -1,7 +1,6 @@
 public class Plain_Decorator : Decorator<string>
 {
     public Plain_Decorator(Deco_Interface<string> component) : base(component) { }
-
     public override string GetText()
     {
         return base.GetText();
